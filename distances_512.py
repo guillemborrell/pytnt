54884d479a30c8248d3dfb44fd7a8a2ff628488f
Guillem Borrell <guillem@torroja.dmt.upm.es>
2013-07-11 11:47:07 +0200

    All the iso thing goes closer to perfection.

--- distances_512.py
+++ distances_512.py
@@ -14,11 +14,12 @@ if __name__ == '__main__':
     hists = list()
 
     #Create the data
-    f = tables.openFile('/data4/guillem/distances/iso384.vort.h5','r')
+    f = tables.openFile('/data4/guillem/distances/iso512.vort.h5','r')
     data = f.root.vorticity_magnitude.read().astype(np.float32)
     N = data.shape[0]
     data = data/np.sqrt(data.mean()**2 + data.std()**2)
-    caseeta = {512: 0.01018, 384: 0.01288, 265: 0.0215}
+    caseeta = {512: 0.01018, 384: 0.01288, 256: 0.0215}
+    caseRel = {512: 170.2, 384: 138.5, 256: 97.6}
     eta = caseeta[N]
     print('Kolmogorov scale', eta)
 
@@ -28,18 +29,22 @@ if __name__ == '__main__':
 
     field = Field(data,x,y,z)
 
-    thresholds = np.logspace(-1,0,5)
+    FRAME = N//8     #Framing that takes into account the boundaries
+
+    thresholds = np.logspace(-0.6,0.3,5)
     for thres in thresholds:
         print('Computing the pdf for the threshold {}'.format(thres))
         surface = field.extract_complete_surface(thres)
         voxels = surface.oversampled_point_list(field)
         
-        sval = data.reshape(N**3)
-        side = (2*(np.sign(data > thres).astype(np.int32))-1).reshape(N**3)
-        trgt = np.empty((N**3,3),dtype=np.double)
+        sval = data[FRAME:N-FRAME,FRAME:N-FRAME,FRAME:N-FRAME].reshape((N-2*FRAME)**3)
+        side = (2*(np.sign(data > thres).astype(np.int32))-1)[FRAME:N-FRAME,FRAME:N-FRAME,FRAME:N-FRAME].reshape((N-2*FRAME)**3)
+        trgt = np.empty(((N-2*FRAME)**3,3),dtype=np.double)
         
         count = 0
-        for i,j,k in product(range(N),range(N),range(N)):
+        for i,j,k in product(range(FRAME,N-FRAME),
+                             range(FRAME,N-FRAME),
+                             range(FRAME,N-FRAME)):
             trgt[count,0] = x[i]
             trgt[count,1] = y[j]
             trgt[count,2] = z[k]
@@ -60,12 +65,17 @@ if __name__ == '__main__':
         semilogx(10**hist[2][1:],
                  np.dot(hist[0].T,hist[1][1:])/np.sum(hist[0],axis=0),
                  linewidth=2)
-        contour(10**hist[2][1:],hist[1][1:],np.log(hist[0]))
+        histplot = np.log(hist[0]/hist[0].sum())
+        contour(10**hist[2][1:],hist[1][1:],histplot/np.abs(histplot.max()))
         semilogx(thresholds[i]*np.ones(hist[2].shape),
                  hist[1],
                  'k--',linewidth=2)
         fig.subplots_adjust(bottom=0.15)
-        xlabel(r'$\omega/\omega^\prime$',fontsize=22)
-        ylabel(r'$d$',fontsize=22)
+        fig.subplots_adjust(top=0.85)
+        xlabel(r'$|\omega|/\omega^\prime$',fontsize=22)
+        ylabel(r'$d/\eta$',fontsize=22)
+        title(r'$Re_\lambda={}$'.format(caseRel[N]),fontsize=22)
+        xlim([0.02,15])
+        ylim([-60,10])
 
     f.close()
--- distances_iso.py
+++ distances_iso.py
@@ -20,6 +20,7 @@ if __name__ == '__main__':
     data = np.sqrt(f.root.Data[:N,:N,:N])
     data = data/np.sqrt(data.mean()**2+ data.std()**2)
     eta = f.root.Kolm_L.read()
+    Rel = 433.0
     print('Kolmogorov scale',eta)
 
     x = np.linspace(0,2*np.pi*N/NTOT,N)/eta
@@ -28,18 +29,22 @@ if __name__ == '__main__':
 
     field = Field(data,x,y,z)
 
-    thresholds = np.logspace(-1,0,5)
+    FRAME = N//8
+
+    thresholds = np.logspace(-0.6,0.3,5)
     for thres in thresholds:
         print('Computing the pdf for the threshold {}'.format(thres))
         surface = field.extract_complete_surface(thres)
         voxels = surface.oversampled_point_list(field)
         
-        sval = data.reshape(N**3)
-        side = (2*(np.sign(data > thres).astype(np.int32))-1).reshape(N**3)
-        trgt = np.empty((N**3,3),dtype=np.double)
+        sval = data[FRAME:N-FRAME,FRAME:N-FRAME,FRAME:N-FRAME].reshape((N-2*FRAME)**3)
+        side = (2*(np.sign(data > thres).astype(np.int32))-1)[FRAME:N-FRAME,FRAME:N-FRAME,FRAME:N-FRAME].reshape((N-2*FRAME)**3)
+        trgt = np.empty(((N-2*FRAME)**3,3),dtype=np.double)
         
         count = 0
-        for i,j,k in product(range(N),range(N),range(N)):
+        for i,j,k in product(range(FRAME,N-FRAME),
+                             range(FRAME,N-FRAME),
+                             range(FRAME,N-FRAME)):
             trgt[count,0] = x[i]
             trgt[count,1] = y[j]
             trgt[count,2] = z[k]
@@ -60,12 +65,17 @@ if __name__ == '__main__':
         semilogx(10**hist[2][1:],
                  np.dot(hist[0].T,hist[1][1:])/np.sum(hist[0],axis=0),
                  linewidth=2)
-        contour(10**hist[2][1:],hist[1][1:],np.log(hist[0]))
+        histplot = np.log(hist[0]/hist[0].sum())
+        contour(10**hist[2][1:],hist[1][1:],histplot/np.abs(histplot.max()))
         semilogx(thresholds[i]*np.ones(hist[2].shape),
                  hist[1],
                  'k--',linewidth=2)
         fig.subplots_adjust(bottom=0.15)
-        xlabel(r'$\omega/\omega^\prime$',fontsize=22)
-        ylabel(r'$d$',fontsize=22)
+        fig.subplots_adjust(top=0.85)
+        xlabel(r'$|\omega|/\omega^\prime$',fontsize=22)
+        ylabel(r'$d/\eta$',fontsize=22)
+        title(r'$Re_\lambda={}$'.format(Rel),fontsize=22)
+        xlim([0.02,15])
+        ylim([-60,10])
 
     f.close()
